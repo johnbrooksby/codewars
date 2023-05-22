165e0dcaf30a9a1baa327280cce7fdee324e82f6
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-05-22 17:11:12 -0600

    adds more functions

--- test15.js
+++ test15.js
@@ -68,45 +68,89 @@
 //   return Math.max(...blockArr);
 // }
 
-function maxSum(arr, range) {
-  let newRange = range;
-  let test1 = -1;
-  if (range.length > 1) {
-    for (let i = 0; i < range.length; i++) {
-      for (let j = i + 1; j < newRange.length; j++) {
-        // console.log(range[i][0]);
-        // console.log(newRange[j][0]);
-        if (
-          range[i][0] <= newRange[j][0] &&
-          range[i][1] >= newRange[j][1] &&
-          test1 >= 0
-        ) {
-          test1 =
-            arr.slice(range[i][0], newRange[j][0] + 1).reduce((a, c) => {
-              return a + c;
-            }) +
-            arr.slice(newRange[j][1], range[i][1] + 1).reduce((a, c) => {
-              return a + c;
-            });
-          if (test1 >= 0) {
-            newRange.splice(j, 1);
-          }
-        }
-      }
+// function maxSum(arr, range) {
+//   let newRange = range;
+//   let test1 = -1;
+//   if (range.length > 1) {
+//     for (let i = 0; i < range.length; i++) {
+//       for (let j = i + 1; j < newRange.length; j++) {
+//         // console.log(range[i][0]);
+//         // console.log(newRange[j][0]);
+//         if (
+//           range[i][0] <= newRange[j][0] &&
+//           range[i][1] >= newRange[j][1] &&
+//           test1 >= 0
+//         ) {
+//           test1 =
+//             arr.slice(range[i][0], newRange[j][0] + 1).reduce((a, c) => {
+//               return a + c;
+//             }) +
+//             arr.slice(newRange[j][1], range[i][1] + 1).reduce((a, c) => {
+//               return a + c;
+//             });
+//           if (test1 >= 0) {
+//             newRange.splice(j, 1);
+//           }
+//         }
+//       }
+//     }
+//   }
+//   let blockArr = [];
+//   for (let i = 0; i < newRange.length; i++) {
+//     blockArr.push(
+//       arr.slice(newRange[i][0], newRange[i][1] + 1).reduce((a, c) => {
+//         return a + c;
+//       })
+//     );
+//   }
+//   return Math.max(...blockArr);
+// }
+
+// console.log(
+//   maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,3]]
+//   )
+// );
+
+// function reverseVowels(str) {
+//   let strArr = str.split("");
+//   const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+//   let arr = [];
+//   for (let i = 0; i < strArr.length; i++) {
+//     if (vowels.includes(strArr[i])) {
+//       arr.unshift(strArr[i]);
+//       strArr.splice(i, 1, "*");
+//     }
+//   }
+//   strArr.join("");
+//   for (let i = 0; i < strArr.length; i++) {
+//     if (strArr[i] === "*") {
+//       for (let j = 0; j < 1; j++) {
+//         strArr[i] = arr[j];
+//         arr.shift()
+//       }
+//     }
+//   }
+//   return strArr.join("");
+// }
+
+// console.log(reverseVowels("abcdefioud"));
+
+var numberFormat = function (number) {
+  let count = 1
+  let num = []
+  for (let i = 0; i < number.toString().length; i++){
+    num.push(number.toString()[i])
+  }
+  console.log(num)
+  for (let i = num.length - 1; i > 1; i--){
+    console.log("number[i]", num[i])
+    count++
+    if (count === 3){
+      count = 0
+      num.splice(i - 1, 0, ",")
     }
   }
-  let blockArr = [];
-  for (let i = 0; i < newRange.length; i++) {
-    blockArr.push(
-      arr.slice(newRange[i][0], newRange[i][1] + 1).reduce((a, c) => {
-        return a + c;
-      })
-    );
-  }
-  return Math.max(...blockArr);
-}
+  return num.join("")
+};
 
-console.log(
-  maxSum([1,-2,3,4,-5,-4,3,2,1],[[1,3]]
-  )
-);
+console.log(numberFormat(1234567))
--- test9.js
+++ test9.js
@@ -89,3 +89,14 @@
 // }
 
 // console.log(p)
+
+// function sayHi() {
+//     console.log(name);
+//     console.log(age);
+//     var name = 'Lydia';
+//     let age = 21;
+//     }
+//     sayHi();
+
+    let str = "((()())((((())()))(()()))(()))(())"
+    console.log(str.length)
