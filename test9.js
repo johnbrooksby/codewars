6a57876ff24b551227adf0e2305b4e5b16dddfd3
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-07-21 23:49:39 -0600

    New tests

--- test9.js
+++ test9.js
@@ -102,3 +102,9 @@
     // console.log(str.length)
 
     // console.log(5++"6")
+
+    var bar = true;
+console.log(bar + 0);
+console.log(bar + "xyz");
+console.log(bar + true);
+console.log(bar + false);
