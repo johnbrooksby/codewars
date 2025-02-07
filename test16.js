97141b9240e0999c508fa7916cb9cb88beb35b1b
Anna Brooksby <johnhbrooksby@yahoo.com>
2025-02-07 01:13:39 -0700

    Function added

--- test16.js
+++ test16.js
@@ -276,3 +276,22 @@
 //     console.log(one+two)
 // },4000)
 
+const fizzbuzz = () => {
+    for (i = 1; i <= 100; i++){
+        if (i % 15 === 0){
+            console.log("Fizz Buzz")}
+        else if(i % 3 === 0){
+            console.log("Fizz")}
+        else if(i % 5 === 0){
+            console.log("Buzz")}
+        else {
+            console.log(i)}
+        }
+        // i % 15 === 0 ? console.log("Fizz Buzz") : 
+        // i % 3 === 0 ? console.log("Fizz") : 
+        // i % 5 === 0 ? console.log("Buzz") : 
+        // console.log(i)}
+}
+
+fizzbuzz()
+
